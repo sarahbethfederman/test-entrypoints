ead29872ba1e918bd19a89b956a26eb8b783698e
Leo Lyu <leo.lyu@lendi.com.au>
2019-01-09 04:32:29 +0000

    Merged in BRAVO-425 (pull request #114)
    BRAVO-425
    
    * draft 1.1
    
    * Include name of LUI icons on example file
    
    * Include name of LUI icons on example file
    
    * change devdependencies
    
    Approved-by: Ally Beaton <ally.beaton@lendi.com.au>
    Approved-by: Maryam Mostajir <maryam.mostajir@lendi.com.au>
    Approved-by: Phillip MacArthur <phillip.macarthur@lendi.com.au>

--- packages/icon/src/icon.example.tsx
+++ packages/icon/src/icon.example.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import styled from 'styled-components';
 import * as Icon from '.';
 import Theme from '@lendi-ui/theme';
+import { Body } from '@lendi-ui/typography';
+import { p, mt } from '@lendi-ui/spacing';
 
 export interface IconProps {
   color: string;
@@ -11,18 +13,25 @@ export interface IconProps {
 }
 
 const Wrapper = styled.span`
-  width: 50px;
-  padding: 10px;
+  width: 200px;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  ${p('xs')};
+`;
+
+const NameWrapper = styled(Body)`
+  ${mt('xxxs')};
 `;
 
 export default () => (
   <Theme>
     {Object.keys(Icon).map((component, i) => {
       const MyComponent: React.SFC<IconProps> = (Icon as any)[component] as React.SFC<IconProps>;
-
       return (
         <Wrapper key={i}>
           <MyComponent color="secondary.800" width="50px" height="50px" />
+          <NameWrapper size="sm">{component}</NameWrapper>
         </Wrapper>
       );
     })}
