09d2255bedf08ba760e8d36956b523f102e16ec2
Anmol Desai <anmol.desai@lendi.com.au>
2019-01-10 23:36:02 +0000

    Merged in BRAVO-331-v2 (pull request #117)
    BRAVO-331 v2
    
    * added danger, fast-blob and check for manifest updating
    
    * test danger
    
    * test danger
    
    * add danger code
    
    * test danger
    
    * test danger
    
    * undo danger changes
    
    * Add check danger in pipeline
    
    * Add check danger in pipeline
    
    * give run permission for check
    
    * make depth full
    
    * add git setup for master
    
    * add git setup for master
    
    * test danger
    
    * undo danger test
    
    * update cache to node, exclude website and add check for yarn lock
    
    * ensure that yarn lock file is not deleted
    
    Approved-by: James Newell <james.newell@lendi.com.au>
    Approved-by: Ally Beaton <ally.beaton@lendi.com.au>

---
+++ ci/check.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+source ./ci/utils/install.sh
+
+source ./ci/utils/setup-git.sh
+
+yarn run check:danger
---
+++ dangerfile.ts
@@ -0,0 +1,74 @@
+import * as path from 'path';
+import * as glob from 'fast-glob';
+import { danger, fail, warn } from 'danger';
+
+function getWorkspaceDirectories(): string[] {
+  const manifest = require('./package.json');
+  const paths = glob.sync<string>(manifest.workspaces.map((pattern) => `${__dirname}/${pattern}/package.json`));
+  return paths.filter((p) => !Boolean(require(p).private)).map((p) => path.relative('.', path.dirname(p)));
+}
+
+function wereFilesInWorkspaceCreatedOrModified(workspaceDirectory: string): boolean {
+  const created = danger.git.created_files.find((file) => file.startsWith(workspaceDirectory));
+  const modified = danger.git.modified_files.find((file) => file.startsWith(workspaceDirectory));
+  return Boolean(created || modified);
+}
+
+function isWorkspaceChangelogUpdated(workspaceDirectory: string): boolean {
+  return (
+    danger.git.created_files.includes(`${workspaceDirectory}/CHANGELOG.md`) ||
+    danger.git.modified_files.includes(`${workspaceDirectory}/CHANGELOG.md`)
+  );
+}
+
+async function isWorkspaceVersionUpdated(workspaceDirectory: string): Promise<boolean> {
+  if (
+    !danger.git.created_files.includes(`${workspaceDirectory}/package.json`) &&
+    !danger.git.modified_files.includes(`${workspaceDirectory}/package.json`)
+  ) {
+    return false;
+  }
+  const { before, after } = await danger.git.diffForFile(`${workspaceDirectory}/package.json`);
+  const beforeJSON = JSON.parse(before);
+  const afterJSON = JSON.parse(after);
+  return beforeJSON.version !== afterJSON.version;
+}
+
+async function expectManifestToBeBumped() {
+  const workspaceDirectories = getWorkspaceDirectories();
+  await Promise.all(
+    workspaceDirectories.map(async (workspaceDirectory) => {
+      const filesInWorkspaceWereCreatedOrModified = wereFilesInWorkspaceCreatedOrModified(workspaceDirectory);
+      const workspaceVersionWasUpdated = await isWorkspaceVersionUpdated(workspaceDirectory);
+      if (filesInWorkspaceWereCreatedOrModified && !workspaceVersionWasUpdated) {
+        fail(
+          `Files in the "${workspaceDirectory}" workspace were modified but the version field in package.json was not.`
+        );
+      }
+    })
+  );
+}
+
+async function expectChangelogToBeCreatedOrUpdated() {
+  const workspaceDirectories = getWorkspaceDirectories();
+  workspaceDirectories.forEach((workspaceDirectory) => {
+    const filesInWorkspaceWereCreatedOrModified = wereFilesInWorkspaceCreatedOrModified(workspaceDirectory);
+    const changelogWasUpdated = isWorkspaceChangelogUpdated(workspaceDirectory);
+    if (filesInWorkspaceWereCreatedOrModified && !changelogWasUpdated) {
+      fail(`Files in the "${workspaceDirectory}" workspace were modified but the CHANGELOG.md was not.`);
+    }
+  });
+}
+
+async function expectYarnLockToNotBeDeleted() {
+  const isYarnLockDeleted = danger.git.deleted_files.includes('yarn.lock');
+  if (isYarnLockDeleted) {
+    fail(`Cannot delete yarn.lock file`);
+  }
+}
+
+(async () => {
+  await expectChangelogToBeCreatedOrUpdated();
+  await expectManifestToBeBumped();
+  await expectYarnLockToNotBeDeleted();
+})();
