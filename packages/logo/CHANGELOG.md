4cfdad02b5176b9eb56007bdeb3a97febe6e1c39
Max Gherman <max.gherman@lendi.com.au>
2019-03-10 22:20:26 +0000

    Merged in BRAVO-387-theme-logo (pull request #167)
    BRAVO-387 theme logo
    
    * Added Domain Logo
    
    * Added yarn lock
    
    * Review fix
    
    * Review fix
    
    Approved-by: Phillip MacArthur <phillip.macarthur@lendi.com.au>
    Approved-by: Ally Beaton <ally.beaton@lendi.com.au>

--- packages/logo/CHANGELOG.md
+++ packages/logo/CHANGELOG.md
@@ -1,3 +1,8 @@
+## [3.0.0] - 2019-03-05 - [BRAVO-387] (https://creditandfinance.atlassian.net/browse/BRAVO-387)
+ 
+### Changed
+- added Domain Logo
+
 ## [2.1.3] - 2019-03-05 - [BRAVO-535] (https://creditandfinance.atlassian.net/browse/BRAVO-535)
  
 ### Changed
--- packages/logo/src/index.tsx
+++ packages/logo/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { withTheme } from 'styled-components';
-import { select } from '@lendi-ui/theme';
-import { LendiLogo, LuiLogo } from '@lendi-ui/icon';
+import { ThemeMap } from '@lendi-ui/theme';
+import { LendiLogo, LuiLogo, DomainLogo } from '@lendi-ui/icon';
 
 export type Variant = 'dark' | 'light';
 
@@ -9,19 +9,22 @@ export interface LogoProps {
   variant?: Variant;
   width?: string;
   height?: string;
-  theme?: any;
+  theme?: ThemeMap;
   className?: string;
 }
 
 class Logo extends React.Component<LogoProps> {
   render() {
-    const { variant = 'dark', width = '160px', height = '100px', className } = this.props;
-    const logoName = select('logo.logoName', 'LUIlogo')(this.props);
+    const { variant = 'dark', width = '160px', height = '100px', className, theme } = this.props;
+
+    const logoName = theme!.logo.logoName;
     const color = variant === 'dark' ? 'secondary.900' : 'shade.0';
 
     switch (logoName) {
       case 'LendiLogo':
         return <LendiLogo color={color} width={width} height={height} className={className} />;
+      case 'DomainLogo':
+        return <DomainLogo color={color} width={width} height={height} className={className} />;
       case 'LUIlogo':
         return <LuiLogo color={color} width={width} height={height} className={className} />;
       default:
