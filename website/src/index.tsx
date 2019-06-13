2c3de1f3d12896ac20e42432ab6b1a1a48d240e0
Mike Cooper <michael.cooper@lendi.com.au>
2019-06-13 16:00:15 +1000

    setup sentry for LUI website

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -2,6 +2,10 @@ import './utils/polyfill';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import { App } from './modules/App';
+import { SENTRY_DSN } from '../utils/consts';
+
+Sentry.init({ dsn: SENTRY_DSN });
 
 ReactDOM.render(<App />, document.getElementById('app'));
