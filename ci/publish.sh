c7c521c0c6ab7de594ec0d74acd6a8b347078d92
Alex Dacre <alex.dacre@lendi.com.au>
2019-12-04 00:40:34 +0000

    Merged in hotfix-alex-is-teh-bomb (pull request #464)
    set loglevel to error to fix maxBuffer error
    
    Approved-by: Malkeet Singh <malkeet.singh@lendi.com.au>
    Approved-by: Leo Lyu <leo.lyu@lendi.com.au>
    Approved-by: Michael Cooper <michael.cooper@lendi.com.au>

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 source ./ci/utils/install.sh
 
-# setup .npmrc in each workspace directory so we can use NPM_TOKEN from env var to publish all the packages 
+# setup .npmrc in each workspace directory so we can use NPM_TOKEN from env var to publish all the packages
 lerna exec cp "$PWD/.npmrc_config" .npmrc
 
 # publish each package version if it isn't already published
-lerna exec --ignore @lendi-ui/reset --ignore @lendi-ui/inline-dropdown -- publish-if-not-published -- --access=public 
+lerna exec --ignore @lendi-ui/reset --ignore @lendi-ui/inline-dropdown -- publish-if-not-published -- --access=public --loglevel=error
