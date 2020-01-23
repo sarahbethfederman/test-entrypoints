6432111b8fd4e788bb7e6884a810c1bcfe9c3ee4
Nick Matenaar <nick.matenaar@lendi.com.au>
2020-01-23 02:36:36 +0000

    Merged in hotfix-master-shell-script (pull request #482)
    Fix syntax error in deploy-lui-website.sh
    
    * Fix syntax error in deploy-lui-website.sh
    
    Approved-by: Alex Dacre
    Approved-by: Michael Cooper
    Approved-by: Malkeet Singh

--- ci/deploy-lui-website.sh
+++ ci/deploy-lui-website.sh
@@ -3,7 +3,7 @@ source ./ci/utils/install.sh
 
 ENVIRONMENT=$1
 
-if [ "$BITBUCKET_BRANCH" == "master"]; then
+if [ "$BITBUCKET_BRANCH" == "master" ]; then
     tag="latest"
 else
     tag=$BITBUCKET_BRANCH
