df6d1fb467313404d55e00234f682572e1b1c5a7
Sarah Federman <sarah.federman@lendi.com.au>
2020-09-03 00:56:04 +0000

    Merged in ci-fix (pull request #548)
    publish script
    
    * publish script

--- ci/publish.sh
+++ ci/publish.sh
@@ -2,7 +2,7 @@
 source ./ci/utils/install.sh
 
 # setup .npmrc in each workspace directory so we can use NPM_TOKEN from env var to publish all the packages
-lerna exec cp "$PWD/.npmrc_config" .npmrc
+manypkg exec cp "$PWD/.npmrc_config" .npmrc
 
 # publish each package version if it isn't already published
-lerna exec --ignore @lendi-ui/reset --ignore @lendi-ui/inline-dropdown -- publish-if-not-published -- --access=public --loglevel=error
+manypkg exec --ignore @lendi-ui/reset --ignore @lendi-ui/inline-dropdown -- publish-if-not-published -- --access=public --loglevel=error
