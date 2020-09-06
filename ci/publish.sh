0f850158a48c9bf77969048e3f39821d104e2ab8
Sarah Federman <sarah.federman@lendi.com.au>
2020-09-06 17:21:47 +1000

    add status check

--- ci/publish.sh
+++ ci/publish.sh
@@ -15,17 +15,19 @@ git checkout
 git config --global user.email $BUILDKITE_BUILD_CREATOR_EMAIL
 git config --global user.name $BUILDKITE_BUILD_CREATOR
 
-git status --porcelain
+# version packages
+yarn changeset version
 
-# # publish each package version if it isn't already published
-# yarn changeset version
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+else 
+  # Here we commit our versioning back to master
+  git add .
+  git commit -m "[ci skip] VERSION_COMMIT_NO_CI"
+  git push
+fi
 
-# # Here we commit our versioning back to master
-# git add .
-# git commit -m "[ci skip] VERSION_COMMIT_NO_CI"
-# git push
+yarn changeset publish
 
-# yarn changeset publish
-
-# # Here we commit our new tags back to master
-# git push --follow-tags
+# Here we commit our new tags back to master
+git push --follow-tags
